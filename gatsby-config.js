7927926744b7616a137e2685107f082efc8fcf12
Bazif Khan <bazif.khan@ibm.com>
2019-06-19 10:47:23 +0100

    head: bootstrap added and intial head component
    Signed-off-by: Bazif Khan <bazif.khan@ibm.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Appsody`
   },
-  plugins: []
+  plugins: [
+    `gatsby-plugin-react-helmet`
+  ]
 }
---
+++ src/components/head.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+export default () => (
+    <Helmet>
+      <meta charset="UTF-8"></meta>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
+      <title>Appsody</title>
+    </Helmet>
+)
