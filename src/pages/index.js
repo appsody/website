220eb61c6ae4cb9db50dd0fec6b43fc1d98d674d
Thomas Leah <thomas@leahfamily.plus.com>
2019-06-19 11:21:08 +0100

    layout: add initial layout component and rebased index
    Signed-off-by: Thomas Leah <thomas@leahfamily.plus.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
-import Navbar from "../components/navbar"
-import Head from "../components/head"
+import Layout from "../components/layout";
+
 const IndexPage = () => (
-
-  <React.Fragment>
-  <Head/>
-  <Navbar/>
-  <h1>Appsody</h1>
-
-
-  </React.Fragment>
+  <Layout>
+    <h1>Appsody</h1>
+  </Layout>
 )
 
 export default IndexPage
