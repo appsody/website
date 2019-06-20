9a811ecb071bc7eefcfc677cacd7f44dbe347a99
Bazif Khan <bazif.khan@ibm.com>
2019-06-20 13:27:15 +0100

    tiles: tiles are responsive now
    Signed-off-by: Bazif Khan <bazif.khan@ibm.com>

---
+++ src/components/tile.js
@@ -0,0 +1,54 @@
+import React from "react";
+// import Head from "./head"
+
+class Tile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+  render() {
+    if (this.state.isToggleOn) {
+      return (
+        <div className="card">
+        <h5 id="tile-head">{this.props.heading}</h5>
+        <p id="tile-text">Description goes in space</p>
+        <a href="/" onClick={this.handleClick} className="btn btn-clear w-70" role="button">Select</a>
+        </div>
+      );
+    } else {
+      return (
+        <div className="card flip">
+        <h5 id="tile-head">{this.props.heading}</h5>
+        <p id="tile-text">Command for CLI</p>
+        <input type="text" name="cli" value="url-link-goes-here" readOnly></input>
+        <br/>
+        <a href="/" onClick={this.handleClick} className="btn btn-primary w-70" role="button">View in GitHub</a>
+        </div>
+      );
+    }
+  }
+}
+// <button onClick={this.handleClick}>
+//   {this.state.isToggleOn ? 'ON' : 'OFF'}
+// </button>
+
+export default Tile;
+
+// export default (props) => (
+//   <React.Fragment>
+//     <Head/>
+
+//   </React.Fragment>
+//
+// )
