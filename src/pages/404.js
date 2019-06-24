0f9f32ecdce348586625072df9ef65d8958ed9ac
Thomas Leah <thomas@leahfamily.plus.com>
2019-06-24 11:49:48 +0100

    404: remove extra navbar
    Signed-off-by: Thomas Leah <thomas@leahfamily.plus.com>

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,10 @@
 import React from "react"
-import Layout from "../components/layout";
 
 const Error404 = () => (
-  <Layout>
+  <React.Fragment>
     <h1 id="error404"> Error 404: Apologies the page you are looking for is not found </h1>
-      <hr></hr>
-  </Layout>
+    <hr></hr>
+  </React.Fragment>
 )
 
 export default Error404;
