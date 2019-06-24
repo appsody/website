851545e91d11dd99e77368cc9c92ede7f560ae2b
Bazif Khan <bazif.khan@ibm.com>
2019-06-24 10:58:15 +0100

    review changes made
    Signed-off-by: Bazif Khan <bazif.khan@ibm.com>

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,10 @@
 import React from "react"
 import Layout from "../components/layout";
-import { Link } from "gatsby";
 
 const Error404 = () => (
   <Layout>
-
-        <h1 id="error404"> Error 404: Apologies the page you are looking for is not found </h1>
-          <hr></hr>
+    <h1 id="error404"> Error 404: Apologies the page you are looking for is not found </h1>
+      <hr></hr>
   </Layout>
 )
 
