ea4ac307f494d22343f664668f0831ae106358cf
Bazif Khan <51790085+Bazif-Khan@users.noreply.github.com>
2019-06-24 11:04:26 +0100

    404: complete error 404 page (#12)
    Signed-off-by: Bazif Khan <bazif.khan@ibm.com>

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout";
+
+const Error404 = () => (
+  <Layout>
+    <h1 id="error404"> Error 404: Apologies the page you are looking for is not found </h1>
+      <hr></hr>
+  </Layout>
+)
+
+export default Error404;
