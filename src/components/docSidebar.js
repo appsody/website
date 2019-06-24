6ca306d3f50db4c85457d63491c7a7453eaf984b
Thomas Leah <thomas@leahfamily.plus.com>
2019-06-24 10:27:40 +0100

    docs: remove boilerplate
    Signed-off-by: Thomas Leah <thomas@leahfamily.plus.com>

--- src/components/docSidebar.js
+++ src/components/docSidebar.js
@@ -57,7 +57,6 @@ export default (props) => (
                         section
                         title
                         path
-                        weight
                     }
                 }
             }
--- src/templates/docTemplate.js
+++ src/templates/docTemplate.js
@@ -2,19 +2,12 @@ import React from "react"
 import { graphql } from "gatsby"
 
 export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
+  data, 
 }) {
-  const { markdownRemark } = data // data.markdownRemark holds our post data
+  const { markdownRemark } = data 
   const { html } = markdownRemark
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-          <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-          />
-      </div>
-    </div>
+    <div dangerouslySetInnerHTML={{ __html: html }} />
   )
 }
 
@@ -24,7 +17,6 @@ export const pageQuery = graphql`
       html
       frontmatter {
         path
-        title
       }
     }
   }
