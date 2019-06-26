d24a9006ae8f9d733980569a2af3afec4c5583c8
Bazif Khan <bazif.khan@ibm.com>
2019-06-26 13:04:06 +0100

    docs page: footer is now at the bottom of the page

--- src/components/docLayout.js
+++ src/components/docLayout.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import Head from "./head"
 import DocNavbar from "./docNavbar"
+import Footer from "../components/footer"
 
 export default ({ children }) => (
     <React.Fragment>
         <Head/>
         <DocNavbar/>
         { children }
+        <Footer/>
     </React.Fragment>
 )
