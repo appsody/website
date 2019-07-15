5c530fd0455e25dec0021280ae02bbbae2b3e0c6
Thomas Leah <thomas@leahfamily.plus.com>
2019-07-15 16:11:17 +0100

    tutorialLink: add final component and style

--- src/components/tutorialLink.js
+++ src/components/tutorialLink.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 const TutorialLink = (props) => (
-    <li className="bg-warning">
+    <li>
         <a href={props.link}>
-            <h2>Package your Node.js app for Cloud with Appsody</h2>
-            <p>hello</p>
+
+            <h2>{props.title}</h2>
+            { (props.time) ? <p className="mb-2"><i class="far fa-clock"></i> {props.time} read</p> : null}
+            <p>{props.abstract}</p>
+            <div className="link-footer">
+                {(props.author) ? <span>by {props.author}</span> : null}
+                <span className="float-right">{props.date}</span>
+            </div>
         </a>
     </li>
 )
