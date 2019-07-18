1a0e4c1ee8f44e83a53345fc5385d513076174de
Sandy Koh <sandykoh99@gmail.com>
2019-07-18 16:05:22 +0100

    pages: added placeholder page on /tutorials before redirect

---
+++ src/pages/tutorials.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const Tutorials = () => (
+    <div id="redirect-page">
+        <div className="container">
+            <section className="landing-section">
+                <div className="row w-100 mx-auto">
+                    <div className="col">
+                        <div className="col"></div>
+                        <div className="w-75 my-4">
+                            <div id="redirect-wrap">
+                                <h2>You are currently being redirected.</h2>
+                                <p id="redirect-link">If you have not been redirected, please click <a href="https://medium.com/appsody">here</a>.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+
+)
+
+export default Tutorials;
