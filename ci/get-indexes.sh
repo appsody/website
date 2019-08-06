120aa8ca036a4ff22a551632310161e8d33d39f3
Thomas Leah <thomas@leahfamily.plus.com>
2019-08-06 14:40:16 +0100

    add script to download production indexes

---
+++ ci/get-indexes.sh
@@ -0,0 +1,8 @@
+# Remove all development indexes
+rm -rf src/data/indexes/*
+
+# Download indexes that will be used on production (appsody.dev)
+cd src/data/indexes
+# Repeat the following command to add new indexes.
+# Incubator
+wget https://github.com/appsody/stacks/releases/download/java-microprofile-v0.2.4/incubator-index.yaml
