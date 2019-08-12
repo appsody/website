a8367d7b9ae1bee543ba8b732048c0b2f7c9e789
tomleah <thomas@leahfamily.plus.com>
2019-08-12 16:20:20 +0100

    tileGrid: dynamically render tiles based on an index.yaml (#155)
    * initial generation
    
    * download yaml from URL
    
    * remove test yaml
    
    * update index url
    
    * add dynamic github links
    
    * add sample stack for development
    
    * update to remove developement indexes
    
    * add script to download production indexes
    
    * comments
    
    * gatsby build from website root
    
    * add shebang
    
    * doc: update release doc for adding tiles to page
    
    * add stable and experimental indexes
    
    * add experimental to init cmd
    
    * addressed changes

---
+++ src/components/tileGrid.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+import Tile from "../components/tile";
+
+const TileGrid = (props) => {
+    
+    const defaultRepo = "/incubator."
+    const tiles = props.stacks.map(stack => {
+        if (stack == null) return null;
+        
+        const templateURL = stack.templates[0].url;
+        const repo = templateURL.split("/").reverse()[0].split(".")[0];
+        const githubURL = `https://github.com/appsody/stacks/tree/master/${repo}/${stack.id}`;
+
+        if (!stack.templates[0].url.includes(defaultRepo)) {
+            return <Tile id={stack.id} heading={stack.name} desc={stack.description} cmd={"appsody init " + repo+"/"+stack.id} github={githubURL}/>
+        }
+        else {
+            return <Tile id={stack.id} heading={stack.name} desc={stack.description} cmd={"appsody init " + stack.id} github={githubURL}/>
+        }
+    });
+
+    return (
+        <div className="container">
+            <div id="application-stacks" className="row mx-auto">
+                {tiles}
+            </div>
+        </div>
+    )
+}
+
+export default () => (
+    <StaticQuery
+      query={graphql`
+        query {
+            allIndexesYaml {
+                nodes {
+                    stacks {
+                        id
+                        name
+                        description
+                        templates {
+                            url
+                        }
+                    }
+                }
+            }
+        }
+      `}
+      render={data => {
+        let stacks = [];
+        data.allIndexesYaml.nodes.forEach(node => {
+            stacks = stacks.concat(node.stacks);
+        });
+        
+        return <TileGrid stacks={stacks}/>
+      }}
+    />
+  )
