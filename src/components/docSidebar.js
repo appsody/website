28acf51ab122b889be0d2eb5dd219b8e02e04a67
Bazif Khan <51790085+Bazif-Khan@users.noreply.github.com>
2019-08-19 11:36:46 +0100

    docSidebar: Docs menu close when clicked on the link in mobile view (#179)
    * docsidebar: const changed to class
    
    * sidebar: In mobile view dropdwon closing when link is clicked
    
    * fixed indentation

--- src/components/docSidebar.js
+++ src/components/docSidebar.js
@@ -1,44 +1,50 @@
-import React from "react";
+import React, { Component } from "react"
 import { Link } from "gatsby"
 import Search from "./search";
 import navStructure from "../../content/docs/sidebar.yaml";
 
-const DocSection = (props) => {
+class DocSection extends Component {
+  closeDocDropdown = () => {
+    document.getElementById("collapsingSideNavbar").classList.remove("show");
+  }
+
+  render() {
     return (
-        <React.Fragment>
-            {
-                (props.title !== "null") ? <h4>{props.title}</h4> : null
-            }
-            <ul>
-                {
-                    props.data.map(doc => <li key={doc.title} className="my-1"><Link activeClassName="active" to={doc.path}>{doc.title}</Link></li>)
-                }
-            </ul>
-        </React.Fragment>
+      <React.Fragment>
+        {
+          (this.props.title !== "null") ? <h4>{this.props.title}</h4> : null
+        }
+        <ul>
+          {
+            this.props.data.map(doc => <li key={doc.title} className="my-1"><Link onClick={this.closeDocDropdown} activeClassName="active" to={doc.path}>{doc.title}</Link></li>)
+          }
+        </ul>
+      </React.Fragment>
     );
+  }
 }
 
 const DocSidebar = () => {
-    let list = [];
-    for (let section of navStructure) {
-        if (section.title == null) {
-            list.push(<DocSection key={section.items[0].title} title={section.title} data={section.items}/>)
-        } else {
-            list.push(<DocSection key={section.title} title={section.title} data={section.items}/>)
-        }
+  let list = [];
+  for (let section of navStructure) {
+    if (section.title == null) {
+      list.push(<DocSection key={section.items[0].title} title={section.title} data={section.items}/>)
+    } else {
+      list.push(<DocSection key={section.title} title={section.title} data={section.items}/>)
     }
-    
-    return (
-        <nav className="navbar-expand-md navbar-light border-bottom" id="docs-sidebar">
-          <Search/>
-          <button className="navbar-toggler float-right" type="button" data-toggle="collapse" data-target="#collapsingSideNavbar">
-              <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="navbar-expand-md collapse d-md-inline" id="collapsingSideNavbar">
-            {list}
-          </div>
-        </nav>
-    )
+  }
+
+  return (
+    <nav className="navbar-expand-md navbar-light border-bottom" id="docs-sidebar">
+      <Search/>
+      <button className="navbar-toggler float-right" type="button" data-toggle="collapse" data-target="#collapsingSideNavbar">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="navbar-expand-md collapse d-md-inline" id="collapsingSideNavbar">
+        {list}
+      </div>
+    </nav>
+  )
 }
 
 export default DocSidebar;
