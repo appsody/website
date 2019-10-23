fb1d654e5d7b9adb9fd3b6f176a52dc4f893d75c
Bazif Khan <bazif.khan@ibm.com>
2019-10-23 15:52:17 +0100

    chevron added

--- src/components/docSidebar.js
+++ src/components/docSidebar.js
@@ -4,12 +4,37 @@ import Search from "./search";
 import navStructure from "../../content/docs/sidebar.yaml";
 import Accordion from 'react-bootstrap/Accordion'
 import Button from 'react-bootstrap/Button'
+import chevronlogo from "../images/chevron.svg";
 
 var accordionIndex = 0;
-
+var lastIndex = null;
 class DocSection extends Component {
-  setAccordionIndex = (index) => {
-    accordionIndex = index
+
+  state = {
+    rotated: false
+  }
+
+  setAccordionIndex = () => {
+    accordionIndex = this.props.index
+  }
+
+  rotateImage() {
+    if (!this.state.rotated || lastIndex !== this.props.index) {
+      var img = document.getElementById(`chevron-${this.props.index}`);
+      img.style.transform = 'rotate(90deg)';
+      this.state.rotated = true
+    } else if (this.state.rotated || lastIndex !== this.props.index){
+      var img = document.getElementById(`chevron-${this.props.index}`);
+      img.style.transform = 'rotate(0)';
+      this.state.rotated = false
+    } 
+  
+    if (lastIndex !== this.props.index && lastIndex !== null){
+      var img = document.getElementById(`chevron-${lastIndex}`);
+      img.style.transform = 'rotate(0)';
+    }
+
+    lastIndex = this.props.index
   }
 
   render() {
@@ -18,14 +43,13 @@ class DocSection extends Component {
       itemList = (
         <>
           <Accordion.Toggle as={Button} variant="link" eventKey={this.props.index}>
-            <h4 className="sidebar-heading-link">{this.props.title}</h4>
+            <h4 onClick={()=>this.rotateImage(this.props.index)} className="sidebar-heading-link">{this.props.title}  <img id={`chevron-${this.props.index}`} src={ chevronlogo } width="10" height="10" className="d-inline-block align-top mt-2" alt="Chevron Logo"></img></h4>
           </Accordion.Toggle>
           <Accordion.Collapse eventKey={this.props.index}>
             <ul>
               {
-              this.props.data.map(doc => <li key={doc.title} className="my-1"><Link className="sidebar-link ml-3" onClick={()=>this.setAccordionIndex(this.props.index)} activeClassName="active" to={doc.path}>{doc.title}</Link></li>)
+              this.props.data.map(doc => <li key={doc.title} className="my-1"><Link className="sidebar-link ml-3" onClick={()=>this.setAccordionIndex()} activeClassName="active" to={doc.path}>{doc.title}</Link></li>)
               }
-
             </ul>
           </Accordion.Collapse>
           </>
