00d5cad7c0860effbb6af2d7943670c8a20939d8
Kye Maloy <kyemaloy2013@gmail.com>
2019-11-06 14:21:37 +0000

    SEO update (Twitter Cards, Doc Titles in metadata) (#340)
    * updated EO
    
    * updated docs to use new header type
    
    * updated SEO component for individual pages
    
    * added -o staging back

--- src/components/docHead.js
+++ src/components/docHead.js
@@ -1,22 +1,20 @@
 import React from "react";
 import * as loadScript from 'simple-load-script';
-import { Helmet } from "react-helmet";
 import SEO from "../components/SEO";
 
-class Head extends React.Component {
+class DocHead extends React.Component {
 
     async componentDidMount() {
         await loadScript('https://code.jquery.com/jquery-3.3.1.slim.min.js', { inBody: true });
         await loadScript('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', { inBody: true });
         await loadScript('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', { inBody: true });
-        await loadScript('https://unpkg.com/web-animations-js@2.3.2/web-animations.min.js', { inBody: true });
     }
 
     render() {
-        return (
-            <SEO></SEO>
-        )
+        return <SEO title={this.props.title}></SEO>
     }
 }
 
-export default Head;
+export default DocHead;
+
+
