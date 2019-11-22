cc7eebb73bf828975b83ad1bec2cd0ec3a93b0d3
David Dunn <dd4dev@gmail.com>
2019-11-22 09:46:43 +0000

    Update quality of favicon (#411)

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: `src/images/favicon.svg`
+        icon: `src/images/favicon.png`
       },
     },
     {
