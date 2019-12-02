ea5a57cab5c6efc9715d6ba3d86923240bb221ba
Bazif Khan <bazif.khan@ibm.com>
2019-12-02 16:30:28 +0000

    transition added on heading

--- src/components/desktop-sidebar-extender.js
+++ src/components/desktop-sidebar-extender.js
@@ -4,14 +4,14 @@ import "../styles/desktop-sidebar-extender.css"
 
 const DesktopSidebarExtender = () => {
   function closeSidebar() {
-      document.getElementById("sidebar").style.marginLeft= "-18.2em";
+      document.getElementById("sidebar").style.marginLeft= "-17.6em";
       document.getElementById("desktop-hamburger-icon" ).style.marginLeft= "-25em"; 
       document.getElementById("documents-window").style.paddingLeft = "8em";    
       document.getElementById("desktopHamburgerOpenbtnId").style.display = "inline";   
       document.getElementById("appsody-sidebar-header").style.writingMode = "vertical-rl";
-      document.getElementById("appsody-sidebar-header").style.marginLeft = "9.5em"; 
+      document.getElementById("appsody-sidebar-header").style.marginLeft = "9.3em"; 
       document.getElementById("docs-sidebar-header").style.writingMode = "vertical-rl";
-      document.getElementById("docs-sidebar-header").style.marginLeft = "9.5em"; 
+      document.getElementById("docs-sidebar-header").style.marginLeft = "9.3em"; 
   }
 
   return (
