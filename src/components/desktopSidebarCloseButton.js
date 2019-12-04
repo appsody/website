42ccb04d499814ec33dcd234aafe8e1156be4cf1
Bazif Khan <51790085+Bazif-Khan@users.noreply.github.com>
2019-12-04 09:37:39 +0000

    Desktop View Sidebar Collapsible (#432)
    * fixes weight issue for lead text on homepage
    
    * sidebar collapsible
    
    * transition added on heading
    
    * responsiveness improved and moved button behind the navbar
    
    * import name changed
    
    * import name changed
    
    * width of closed sidebar increased
    
    * change to pass travis
    
    * button updated
    
    * extra css removed
    
    * height of hide sidebar button increased
    
    * minor change in position of hamburger button

---
+++ src/components/desktopSidebarCloseButton.js
@@ -0,0 +1,30 @@
+import React from "react"
+import "../styles/desktopSidebarCloseButton.css"
+
+
+const DesktopSidebarExtender = () => {
+  function closeSidebar() {
+      window.sidebarOpen = false;
+      document.getElementById("accordion").style.display="none"
+      document.getElementById("sidebar").style.marginLeft= "-17em";
+      document.getElementById("desktop-hamburger-icon" ).style.marginLeft= "-25em"; 
+      document.getElementById("documents-window").style.paddingLeft = "8em";    
+      document.getElementById("desktopHamburgerOpenbtnId").style.display = "inline";   
+      document.getElementById("appsody-sidebar-header").style.writingMode = "vertical-rl";
+      document.getElementById("appsody-sidebar-header").style.marginLeft = "9.2em"; 
+      document.getElementById("docs-sidebar-header").style.writingMode = "vertical-rl";
+      document.getElementById("docs-sidebar-header").style.marginLeft = "9.2em"; 
+      document.getElementById("desktopHamburgerOpenbtnId").style.display="flex"
+  }
+
+  return (
+      <section onClick={() => closeSidebar()} className="desktop-sidebar-hamburger" id="desktop-hamburger-icon">
+        <span></span>
+        <span></span>
+        <span></span>
+        <p>Hide Sidebar</p>
+      </section>
+  )
+}
+
+export default DesktopSidebarExtender
