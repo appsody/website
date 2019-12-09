26d49169775dd06ca3d901eb2d0aa7a806791c99
Bazif Khan <bazif.khan@ibm.com>
2019-12-09 10:28:10 +0000

    stacks moved to stacks stacks page

---
+++ src/pages/stacks.js
@@ -0,0 +1,11 @@
+import React from "react"
+import TileGrid from "../components/tileGrid";
+
+
+const Stacks = () => (
+    <React.Fragment>
+       <TileGrid/>
+    </React.Fragment>
+)
+
+export default Stacks;
