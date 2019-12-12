a2f1afac9e1cf4a821384552504faf34c045a23e
Bazif Khan <bazif.khan@ibm.com>
2019-12-12 16:20:33 +0000

    extra code removed

--- src/pages/stacks.js
+++ src/pages/stacks.js
@@ -2,30 +2,16 @@ import React from "react"
 import TileGrid from "../components/tileGrid";
 import Layout from "../components/layout"
 
-export default class IndexPage extends React.Component {
-    state = {
-        firstName: "",
-        lastName: false,
-      }
+const StackPage = () => (
+    <Layout>
+        <section className="stacks-section">
+            <div className="stacks">
+                <h2>Application Stacks</h2>
+                <p className="px-5">Select the application stack to view further details on GitHub or copy the command to start using the stack with our CLI.</p>
+                <TileGrid apple="java"/>
+            </div>
+        </section>
+    </Layout>
+)
 
-
-
-      filter() {
-          return "java";
-      }
-
-    render() {
-        return (
-            <Layout>
-                
-                <section className="stacks-section">
-                    <div className="stacks">
-                        <h2>Application Stacks</h2>
-                        <p className="px-5">Select the application stack to view further details on GitHub or copy the command to start using the stack with our CLI.</p>
-                        <TileGrid apple="java"/>
-                    </div>
-                </section>
-            </Layout>
-        );
-    }
-  }
+export default StackPage
