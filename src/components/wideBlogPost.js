58b2e8beacbe8b65cc64e10320c2c84e83f45aa8
Bazif Khan <bazif.khan@ibm.com>
2020-02-24 16:13:38 +0000

    Search results show a blank instead of the page title

--- src/components/wideBlogPost.js
+++ src/components/wideBlogPost.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "gatsby";
 
-const WideBlogPost = ({title, subtext, author, date, image, slug}) => (
+const WideBlogPost = ({title, subtext, author, date, slug}) => (
 
     <Link className="blog-link" to={slug}>
     <div className="wide-blog-container">
         <div className="wide-blog-image-container">
-            <img className="wide-blog-image" src={"https://miro.medium.com/max/2800/1*AmrAV709WQFlXgs4R4zZEw.jpeg"}/>
+            <img className="wide-blog-image" src={"https://miro.medium.com/max/2800/1*AmrAV709WQFlXgs4R4zZEw.jpeg"} alt="Blog post pic"/>
         </div>
         <h2 className="wide-blog-title">{title}</h2>
         <p className="wide-blog-subtext">{subtext}</p>
